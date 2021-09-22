13ba66525c59e0a5ede30fea138ce5d9a416f31a
florian.charpentier <florian.charpentier@epita.fr>
2021-09-22 15:19:48 +0200

    refacto enum

--- models/container_status.go
+++ models/container_status.go
@@ -15,28 +15,27 @@ const (
 	BadState
 )
 
+var toString = map[ContainerStatus]string{
+	Configured: "configured",
+	Created:    "created",
+	Running:    "running",
+	Stopped:    "stopped",
+	Paused:     "paused",
+	Exited:     "exited",
+	Removing:   "removing",
+	Unknown:    "unknown",
+}
+
 func NewContainerStatus(s string) ContainerStatus {
-	switch s {
-	case "configured":
-		return Configured
-	case "created":
-		return Created
-	case "running":
-		return Running
-	case "stopped":
-		return Stopped
-	case "paused":
-		return Paused
-	case "exited":
-		return Exited
-	case "removing":
-		return Removing
-	case "unknown":
-		return Unknown
+	for k, v := range toString {
+		if v == s {
+			return k
+		}
 	}
+
 	return BadState
 }
 
 func (p ContainerStatus) String() string {
-	return []string{"configured", "created", "running", "stopped", "paused", "exited", "removing", "unknown", "bad state"}[p]
+	return toString[p]
 }
